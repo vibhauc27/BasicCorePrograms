2b70081b107ffa2bc0cafc1c9f5af2967ce77090
vibha <vibhauc@gmail.com>
2022-02-15 20:54:56 +0530

    [Vibha]Added:Leap year solution

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -6,11 +6,9 @@ namespace BasicCoreProgram
     {
         public static void Main(string[] args)
         {
-            //int year;
+           
             Console.WriteLine("Enter the Year : ");
             int year = Convert.ToInt32(Console.ReadLine());
-            //Scanner sc = new Scanner(System.in);
-            //year = sc.nextInt();
 
             if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
                 Console.WriteLine("This is a leap year" + year);
