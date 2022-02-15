eff5c3a2a27cc914cfed6eef8287f5d7666bb561
vibha <vibhauc@gmail.com>
2022-02-15 20:47:27 +0530

    First Commit

---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+        public static void Main(string[] args)
+        {
+            //int year;
+            Console.WriteLine("Enter the Year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            //Scanner sc = new Scanner(System.in);
+            //year = sc.nextInt();
+
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                Console.WriteLine("This is a leap year" + year);
+            else
+                Console.WriteLine("This is not a leap year" + year);
+        }
+    }
+}
